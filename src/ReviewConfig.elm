82c23d576f2c3b2369b8ec682c63d202d87103d0
James Carlson <jxxcarlson@gmail.com>
2024-05-11 19:28:43 -0400

    updates to fix code

--- src/ReviewConfig.elm
+++ src/ReviewConfig.elm
@@ -6,5 +6,5 @@ import Review.Rule exposing (Rule)
 
 config : List Rule
 config =
-    [ MagicToken.AddToType.makeAddToTypeRule "FrontendMsg" "Bar" "| Bar"
+    [ MagicToken.AddToType.makeAddToTypeRule "FrontendMsg" "Bar" "\n  | Bar"
     ]
