400eacbf45590c497bb0e9e4171a8f75d5ac998a
James Carlson <jxxcarlson@gmail.com>
2024-05-13 14:41:37 -0400

    update ReviewConfig to test modifying init code

--- elm-review-add-code/src/ReviewConfig.elm
+++ elm-review-add-code/src/ReviewConfig.elm
@@ -3,18 +3,20 @@ module ReviewConfig exposing (config)
 import MagicToken.AddToCaseStatementInFunction
 import MagicToken.AddToType
 import MagicToken.AddToTypeAlias
+import MagicToken.UpdateInit
 import Review.Rule exposing (Rule)
 
 
 config : List Rule
 config =
     [ -- MagicToken.AddToType.makeAddToTypeRule "FrontendMsg" "Bar"
-      MagicToken.AddToType.makeAddToTypeRule "ToBackend" "ResetCounter"
+      -- MagicToken.AddToType.makeAddToTypeRule "ToBackend" "ResetCounter"
+      MagicToken.UpdateInit.makeRule "Backend" "init" "message" "hohoho!"
 
     -- MagicToken.AddToTypeAlias.makeAddToTypeAliasRule "FrontendModel" "quot: String"
-    , MagicToken.AddToCaseStatementInFunction.makeAddToCaseStatementInFunctionRule
-        "Backend"
-        "updateFromFrontend"
-        "ResetCounter"
-        "( { model | counter = 0 }, broadcast (CounterNewValue 0 clientId) )"
+    --, MagicToken.AddToCaseStatementInFunction.makeAddToCaseStatementInFunctionRule
+    --    "Backend"
+    --    "updateFromFrontend"
+    --    "ResetCounter"
+    --    "( { model | counter = 0 }, broadcast (CounterNewValue 0 clientId) )"
     ]
