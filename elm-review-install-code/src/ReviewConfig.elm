8688cc689efc2bb4f76bdd81c40f7fb3a6eec0a6
James Carlson <jxxcarlson@gmail.com>
2024-05-14 01:31:42 -0400

    Check modulen|Name. . . .

--- elm-review-install-code/src/ReviewConfig.elm
+++ elm-review-install-code/src/ReviewConfig.elm
@@ -7,6 +7,10 @@ import Install.TypeVariant
 import Review.Rule exposing (Rule)
 
 
+{-| The first three rules in the config file add variants to somoe
+of the types defined in module `Types`. The last two rules add
+code to the 'init' and `updateFromFrontend` function of the`Backend` module.
+-}
 config : List Rule
 config =
     [ Install.TypeVariant.makeRule "Types" "FrontendMsg" "Bar"
